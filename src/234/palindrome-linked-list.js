d59854a2cb8af2ec1619ce2d2e4476958528e2ee
Thiago Marques <megathiago@gmail.com>
2023-01-18 12:58:38 -0300

    Solved 234 - palindrome-linked-list

--- src/234/palindrome-linked-list.js
+++ src/234/palindrome-linked-list.js
@@ -4,62 +4,59 @@ import { arrayToListNode, ListNode } from '../helpers/listNode'
  * @param {ListNode} head
  * @return {boolean}
  */
-// export function isPalindrome(head) {
-// 	const array = [head.val]
-// 	while (head.next) {
-// 		head = head.next
-// 		array.push(head.val)
-// 	}
-
-// 	for (let i = 0; i < array.length / 2; i++) {
-// 		const first = array[i]
-// 		const second = array.at(-i - 1)
-// 		if (first !== second) return false
-// 	}
-
-// 	return true
-// }
-
 export function isPalindrome(head) {
-	let slow = head,
-		fast = head,
-		prev = null,
-		temp,
-		slowBackwards = head
-		prev = slow
-		prev.next = null
-
-	while (fast?.next) {
-		slowBackwards.next = prev
-		prev = slow
-
-		slow = slow.next
-		fast = fast.next.next
-		
-		slowBackwards = slow
-  }
-	return slowBackwards
-  
-	slow = slow.next
-
-	while (slow) {
-		temp = slow.next
-		slow.next = prev
-		prev = slow
-		slow = temp
+	const array = [head.val]
+	while (head.next) {
+		head = head.next
+		array.push(head.val)
 	}
 
-	fast = head
-	slow = prev
-
-	while (slow) {
-		if (fast.val !== slow.val) return false
-
-		fast = fast.next
-		slow = slow.next
+	for (let i = 0; i < array.length / 2; i++) {
+		const first = array[i]
+		const second = array.at(-i - 1)
+		if (first !== second) return false
 	}
 
 	return true
 }
 
-isPalindrome(arrayToListNode([1,2,3,4])) //?
+/**
+ * Fastest solution without using array
+ * 
+ * See full explanation on: {@link https://leetcode.com/problems/palindrome-linked-list/solutions/1137027/js-python-java-c-easy-floyd-s-reversal-solution-w-explanation/ LeetCode}
+ * @param {ListNode} head 
+ */
+// export function isPalindrome(head) {
+// 	let slow = head,
+// 		fast = head,
+// 		prev,
+// 		temp
+
+// 	while (fast?.next) {
+// 		fast = fast.next.next
+// 		slow = slow.next
+// 	}
+
+// 	prev = slow
+// 	slow = slow.next
+// 	prev.next = null
+
+// 	while (slow) {
+// 		temp = slow.next
+// 		slow.next = prev
+// 		prev = slow
+// 		slow = temp
+// 	}
+
+// 	fast = head
+// 	slow = prev
+
+// 	while (slow) {
+// 		if (fast.val !== slow.val) return false
+
+// 		fast = fast.next
+// 		slow = slow.next
+// 	}
+
+// 	return true
+// }
